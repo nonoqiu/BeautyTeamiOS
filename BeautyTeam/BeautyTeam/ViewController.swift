02911057b4855b49f35a6f9adb5c6c9928c3644d
lalxyy <hkyla00@126.com>
2016-03-29 10:36:58 +0800

    Create project

---
+++ BeautyTeam/BeautyTeam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeautyTeam
+//
+//  Created by 李昂 on 3/29/16.
+//  Copyright © 2016 Shenyang Obisoft Technology Co.Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
