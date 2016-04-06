8c4b6a32dbf38b613676f21f0b7337b5cad01f76
Carl Lee <hkyla00@gmail.com>
2016-04-07 07:54:31 +0800

    finish the welcome API

---
+++ BeautyTeam/BeautyTeam/NetworkAllowUntrustedCert.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkAllowUntrustedCert.swift
+//  BeautyTeam
+//
+//  Created by 李昂 on 4/7/16.
+//  Copyright © 2016 Shenyang Obisoft Technology Co.Ltd. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+let manager: Manager = {
+    let serverTrustPolicies: [String: ServerTrustPolicy] = [
+        "www.obisoft.com.cn": .DisableEvaluation
+    ]
+    
+    let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+    configuration.HTTPAdditionalHeaders = Alamofire.Manager.defaultHTTPHeaders
+    
+    return Alamofire.Manager(configuration: configuration,
+                             serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies))
+}()
--- BeautyTeam/BeautyTeam/ViewController.swift
+++ BeautyTeam/BeautyTeam/ViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager.request(.GET, "https://www.obisoft.com.cn/api/").responseJSON(completionHandler: {
+            resp in
+            
+            print(resp.request)
+            print(resp.response)
+            print(resp.data)
+            print(resp.result)
+            
+            guard let JSON = resp.result.value else {
+                return
+            }
+            print(JSON)
+        })
         // Do any additional setup after loading the view, typically from a nib.
     }
 
