1e818f1cebae0c699e70b17d593690bbfca64a7f
Carl Lee <hkyla00@gmail.com>
2016-04-23 20:02:28 +0800

    Suddenly unexpectedly want to have a commit

--- BeautyTeam/BeautyTeam/NoticeCell.swift
+++ BeautyTeam/BeautyTeam/NoticeCell.swift
@@ -20,15 +20,47 @@ class NoticeCell: UITableViewCell {
     var locationImageView: UIImageView?
     var locationLabel: UILabel?
     
-    init(title: String, project: String?, time: NSDate, location: String, reuseIdentifier: String) {
+    init(reuseIdentifier: String) {
         super.init(style: .Default, reuseIdentifier: reuseIdentifier)
         
+        let fontAwesomeCGSize = CGSize(width: 12, height: 12)
+        
         self.titleLabel = UILabel(frame: CGRectMake(10, 10, 200, 20))
-        self.titleLabel?.text = title
         self.titleLabel?.font = UIFont.systemFontOfSize(12)
         
         self.projectImageView = UIImageView(frame: CGRectMake(10, 40, 12, 12))
-        self.projectImageView?.image = UIImage.fontAwesomeIconWithName(.Tag, textColor: UIColor(red: 80/255, green: 227/255, blue: 194/255, alpha: 1), size: CGSize(width: 12, height: 12))
+        self.projectImageView?.image = UIImage.fontAwesomeIconWithName(.Tag, textColor: UIColor(red: 80/255, green: 227/255, blue: 194/255, alpha: 1), size: fontAwesomeCGSize)
+        
+        self.projectLabel = UILabel(frame: CGRectMake(40, 40, 300, 12))
+        self.projectLabel?.font = UIFont.systemFontOfSize(10)
+        
+        self.timeImageView = UIImageView(frame: CGRectMake(10, 60, 12, 12))
+        self.timeImageView?.image = UIImage.fontAwesomeIconWithName(.ClockO, textColor: UIColor(red: 74/255, green: 144/255, blue: 226/255, alpha: 1), size: fontAwesomeCGSize)
+        
+        self.timeLabel = UILabel(frame: CGRectMake(10, 60, 200, 12))
+        self.timeLabel?.font = UIFont.systemFontOfSize(10)
+        
+        self.emergencyTimeLabel = UILabel(frame: CGRectMake(10, 320, 100, 12))
+        self.emergencyTimeLabel?.font = UIFont.systemFontOfSize(10)
+        
+        self.locationImageView = UIImageView(frame: CGRectMake(10, 80, 12, 12))
+        self.locationImageView?.image = UIImage.fontAwesomeIconWithName(.LocationArrow, textColor: UIColor(red: 74/255, green: 144/255, blue: 226/255, alpha: 1), size: fontAwesomeCGSize)
+        
+        self.locationLabel = UILabel(frame: CGRectMake(10, 80, 300, 12))
+        self.locationLabel?.font = UIFont.systemFontOfSize(10)
+    }
+    
+    func assignValue(title: String, project: String?, time: NSDate, location: String) {
+        self.titleLabel?.text = title
+        
+        if (project != nil) {
+            let projectAttributedStr = NSMutableAttributedString(string: project!)
+            projectAttributedStr.setAttributes([
+                NSForegroundColorAttributeName: UIColor(red: 80/255, green: 227/255, blue: 194/255, alpha: 1)
+                ], range: NSMakeRange(0, (project?.characters.count)!))
+            self.projectLabel?.attributedText = projectAttributedStr
+        }
+        
         
     }
     
