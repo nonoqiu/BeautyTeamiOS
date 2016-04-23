8460e2e754097e9eda115644a57d5decb10806c8
Carl Lee <hkyla00@gmail.com>
2016-04-23 09:55:57 +0800

    Initialize notice cell

--- BeautyTeam/BeautyTeam/NoticeCell.swift
+++ BeautyTeam/BeautyTeam/NoticeCell.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import FontAwesome_swift
 
 class NoticeCell: UITableViewCell {
     
-    var headerImage: UIImageView?
+    var titleLabel: UILabel?
+    var projectImageView: UIImageView?
+    var projectLabel: UILabel?
+    var timeImageView: UIImageView?
+    var timeLabel: UILabel?
+    var emergencyTimeLabel: UILabel?
+    var locationImageView: UIImageView?
+    var locationLabel: UILabel?
     
+    init(title: String, project: String?, time: NSDate, location: String, reuseIdentifier: String) {
+        super.init(style: .Default, reuseIdentifier: reuseIdentifier)
+        
+        self.titleLabel = UILabel(frame: CGRectMake(10, 10, 200, 20))
+        self.titleLabel?.text = title
+        self.titleLabel?.font = UIFont.systemFontOfSize(12)
+        
+        self.projectImageView = UIImageView(frame: CGRectMake(10, 40, 12, 12))
+        self.projectImageView?.image = UIImage.fontAwesomeIconWithName(.Tag, textColor: UIColor(red: 80/255, green: 227/255, blue: 194/255, alpha: 1), size: CGSize(width: 12, height: 12))
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
